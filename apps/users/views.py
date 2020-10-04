c589f9ee28457e9b7b940aacd63db8127554e614
Corley <cutercorleytd@gmail.com>
2020-10-04 17:09:05 +0800

    V1.0

---
+++ apps/users/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views import View
+
+class RegisterView(View):
+    '''用户注册'''
+    def get(self, request):
+        '''提供用户注册页面'''
+        return render(request, 'register.html')
